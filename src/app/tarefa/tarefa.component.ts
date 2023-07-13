880f6ddd2de338a9927a6b8958c576a051cc0843
unknown <jfs102019@hotmailcom>
2023-07-13 10:06:00 -0300

    taks: adicionar tarefa

--- src/app/tarefa/tarefa.component.ts
+++ src/app/tarefa/tarefa.component.ts
@@ -1,10 +1,24 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-tarefa',
   templateUrl: './tarefa.component.html',
   styleUrls: ['./tarefa.component.css']
 })
-export class TarefaComponent {
+export class TarefaComponent implements OnInit {
+  ListaTarefas: any[] = [];
 
+  constructor() {}
+
+  ngOnInit(): void {
+    this.ListaTarefas = [
+      { id: 0, nome: 'Lavar o Carro', concluida: false },
+      { id: 1, nome: 'Ir ao mercado', concluida: true },
+      { id: 2, nome: 'Lavar louça', concluida: false }
+    ];
+  }
+
+  adicionar(nomeTarefa:string){
+     this.ListaTarefas.push({id: this.ListaTarefas.length, nome: nomeTarefa, concluida: false})
+  }
 }
